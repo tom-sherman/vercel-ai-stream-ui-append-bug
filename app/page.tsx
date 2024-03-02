e85041d0d1557135fa4bc2f4ee94b2c813b136ba
Tom Sherman <the.tomsherman@gmail.com>
2024-03-02 02:03:30 +0000

    Repro

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,17 @@
+import { createStreamableUI } from "ai/rsc";
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null;
+  const streamable = createStreamableUI();
+
+  setTimeout(() => {
+    streamable.append(<p>Foo</p>);
+    setTimeout(() => {
+      streamable.append(<p>Bar</p>);
+      // Uncommenting the line below will fix the issue
+      // streamable.done();
+    }, 1000);
+  }, 1000);
+
+  return streamable.value;
 }
